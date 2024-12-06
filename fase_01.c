89403573e0399758e58ce4a51540f39e27564fa0
jhonnydrummer <arantes.designergrafico@gmail.com>
2024-12-06 16:37:42 +0000

    algumas funções

---
+++ fase_01.c
@@ -0,0 +1,5 @@
+//
+// Created by jhonn on 06/12/2024.
+//
+
+#include "fase_01.h"
---
+++ fase_01.h
@@ -0,0 +1,8 @@
+//
+// Created by jhonn on 06/12/2024.
+//
+
+#ifndef FASE_01_H
+#define FASE_01_H
+
+#endif //FASE_01_H
